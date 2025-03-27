8fb740dfa17ac5eb57e064e6d0fa75e77adfbade
daruuu <thedaruny16@gmail.com>
2025-03-27 19:10:55 +0100

    add unsersioned files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.daruuu.connectfourgame {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.daruuu.connectfourgame to javafx.fxml;
+    exports com.daruuu.connectfourgame;
+}
